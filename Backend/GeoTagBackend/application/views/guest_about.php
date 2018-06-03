92219dc93f7a6ff18ad363c547609977b662fcd4
Jakov Jezdić <jakovjezdic@gmail.com>
2018-06-03 23:33:30 +0200

    Sredjivanje about-a, nebitno

--- Backend/GeoTagBackend/application/views/guest_about.php
+++ Backend/GeoTagBackend/application/views/guest_about.php
@@ -13,8 +13,16 @@
 								<div class="card" style="margin:10px;padding:40px; border-width: 4px">
                                                                     <h5><strong>GeoTag</strong></h5>
                                                                 <p>is application made for purposes of faculty course Principles of Software Engineering.<br>
-                                                                It allows all travel enthusiasts to share their journeys with rest of the world end
+                                                                It allows all travel enthusiasts to share their journeys with rest of the world and
                                                                 explore new destinations from their living room.</p>
+                                                                <p>
+                                                                    Used for building <i>GeoTag<i>: <br>
+                                                                            
+                                                                </p>
+                                                                <ul>
+                                                                    <li>Frontend: HTML, CSS, JavaScript</li>
+                                                                    <li>Backend: PHP, CodeIgniter, Ajax, <a href="https://developers.google.com/maps/documentation/javascript/tutorial">GoogleMaps API</a>, MySQL</li>
+                                                                </ul>
                                                                 <a href="https://github.com/jakovj/GeoTag">More ...</a>
 								</div>
 							</div>
@@ -24,14 +32,60 @@
 							<img src="<?php echo base_url()?>img/DP-team.png" width="150px">
 							<div class="media-body">
 								<div class="card" style="margin:10px;padding:40px; border-width: 4px">
-								<h5><strong>DiningPhilosophers</strong></h5><br>
-                                                                <p>
-                                                                    Jakov Jezdić <i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-"As far as the laws of mathematics refer to reality, they are not certain; 
-                                                                        and as far as they are certain, they do not refer to reality."</i><br><br>
-                                                                    Marko Hudomal <i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-"Goals transform a random walk into a chase."</i><br><br>
-                                                                    Miloš Matijašević <i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-"Together we can change the world, just one random act of kindness at a time."</i><br><br>
-                                                                    Dejan Ćirić <i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-"Computers themselves, and software yet to be developed, will revolutionize the way we learn."</i>
-                                                                </p>									
+                                                                    <h5><strong>DiningPhilosophers &nbsp;&nbsp;</strong> <i>development team</i></h5><br>
+                                                                <table>
+                                                                    <tr>
+                                                                        <td>
+                                                                            Jakov Jezdić 
+                                                                        </td>
+                                                                        <td>
+                                                                            <i>-"As far as the laws of mathematics refer to reality, they are not certain; 
+                                                                            and as far as they are certain, they do not refer to reality."</i>                                                               
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td><td><td><td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td><td><td><td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            Marko Hudomal
+                                                                        </td>
+                                                                        <td>
+                                                                            <i>-"Goals transform a random walk into a chase."</i>
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td><td><td><td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td><td><td><td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            Miloš Matijašević &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                                                        </td>
+                                                                        <td>
+                                                                            <i>-"Together we can change the world, just one random act of kindness at a time."</i>
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td><td><td><td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td><td><td><td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            Dejan Ćirić 
+                                                                        </td>
+                                                                        <td>
+                                                                            <i>-"Computers themselves, and software yet to be developed, will revolutionize the way we learn."</i>
+                                                                        </td>
+                                                                    </tr>
+                                                                </table>             									
 								</div>
 							</div>
 						</div>
