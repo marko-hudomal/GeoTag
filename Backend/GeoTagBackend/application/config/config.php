3fe8ee17d2299d894f8eb9aa0b048549804a43bc
Marko Hudomal <38257103+marko-hudomal@users.noreply.github.com>
2018-06-06 01:29:31 +0200

    nek stoje u bazi slike, ali nisu realizovane

--- Backend/GeoTagBackend/application/config/config.php
+++ Backend/GeoTagBackend/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/GeoTag/Backend/GeoTagBackend';
+$config['base_url'] = 'http://localhost:5050/GeoTag/Backend/GeoTagBackend';
 
 /*
 |--------------------------------------------------------------------------
