a72e87535114af61b07e2cc2d72ad69933ead28f
Marko Hudomal <38257103+marko-hudomal@users.noreply.github.com>
2018-06-06 01:43:03 +0200

    Erroe vratio zbog hostinga

--- Backend/GeoTagBackend/application/views/errors/html/error_404.php
+++ Backend/GeoTagBackend/application/views/errors/html/error_404.php
@@ -1,16 +1,64 @@
-
-<!-- Marko Hudomal 0112/15 -->
-
-<!DOCTYPE html>
-<html>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
 <head>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="icon" href="<?php echo base_url()?>img/logo.png">
-	<title>404 Error</title>
+<meta charset="utf-8">
+<title>404 Page Not Founde</title>
+<style type="text/css">
+
+::selection { background-color: #E13300; color: white; }
+::-moz-selection { background-color: #E13300; color: white; }
+
+body {
+	background-color: #fff;
+	margin: 40px;
+	font: 13px/20px normal Helvetica, Arial, sans-serif;
+	color: #4F5155;
+}
+
+a {
+	color: #003399;
+	background-color: transparent;
+	font-weight: normal;
+}
+
+h1 {
+	color: #444;
+	background-color: transparent;
+	border-bottom: 1px solid #D0D0D0;
+	font-size: 19px;
+	font-weight: normal;
+	margin: 0 0 14px 0;
+	padding: 14px 15px 10px 15px;
+}
+
+code {
+	font-family: Consolas, Monaco, Courier New, Courier, monospace;
+	font-size: 12px;
+	background-color: #f9f9f9;
+	border: 1px solid #D0D0D0;
+	color: #002166;
+	display: block;
+	margin: 14px 0 14px 0;
+	padding: 12px 10px 12px 10px;
+}
+
+#container {
+	margin: 10px;
+	border: 1px solid #D0D0D0;
+	box-shadow: 0 0 8px #D0D0D0;
+}
+
+p {
+	margin: 12px 15px 12px 15px;
+}
+</style>
 </head>
 <body>
-	<div class="container-fluid" style="margin:15%; margin-top:1%; margin-bottom:1%; align-content:center;">
-		<img src='<?php echo base_url()?>img/404_background.jpg' alt="Error 404" style="width:70%">
+	<div id="container">
+		<h1><?php echo $heading; ?></h1>
+		<?php echo $message; ?>
 	</div>
 </body>
-<div hidden>
+</html>
