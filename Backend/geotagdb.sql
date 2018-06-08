04561be3092b821bac1abdc961263666b509aea5
Jakov Jezdić <jakovjezdic@gmail.com>
2018-06-08 15:47:05 +0200

    Export baze sa hostinga

--- Backend/geotagdb.sql
+++ Backend/geotagdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jun 06, 2018 at 10:17 AM
--- Server version: 5.7.21
--- PHP Version: 5.6.35
+-- Host: localhost:3306
+-- Generation Time: Jun 08, 2018 at 01:45 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,144 +19,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `geotagdb`
+-- Database: `id6095028_geotagdb`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `destination`
---
-
-DROP TABLE IF EXISTS `destination`;
-CREATE TABLE IF NOT EXISTS `destination` (
-  `idDest` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `longitude` double NOT NULL,
-  `latitude` double NOT NULL,
-  `pending` tinyint(1) NOT NULL,
-  `country` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`idDest`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `destination`
---
-
-INSERT INTO `destination` (`idDest`, `name`, `longitude`, `latitude`, `pending`, `country`) VALUES
-(12, 'Beograd', 20.45434737375001, 44.77446920344899, 0, 'Srbija'),
-(16, 'Titel', 20.285432578828136, 45.2114980600597, 0, 'Srbija'),
-(17, 'Vrsac', 21.29480147531251, 45.120482958475634, 0, 'Srbija'),
-(20, 'Pancevo', 20.65210128000001, 44.87090149651767, 0, 'Srbija'),
-(21, 'Rimini', 12.56753707101575, 44.08999561760694, 0, 'Italija'),
-(25, 'Grac', 15.46655440500001, 47.051826810104906, 1, 'Austrija'),
-(26, 'Pariz', 2.3269059675000108, 48.919290244349654, 1, 'Francuska');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `image`
---
-
-DROP TABLE IF EXISTS `image`;
-CREATE TABLE IF NOT EXISTS `image` (
-  `idImg` int(11) NOT NULL AUTO_INCREMENT,
-  `img` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`idImg`)
-) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `image`
---
-
-INSERT INTO `image` (`idImg`, `img`) VALUES
-(72, 'pic_grand.jpg'),
-(73, 'pic_grand1.jpg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `request`
---
-
-DROP TABLE IF EXISTS `request`;
-CREATE TABLE IF NOT EXISTS `request` (
-  `idReq` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `username` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `idRev` int(11) DEFAULT NULL,
-  `idDest` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idReq`),
-  KEY `username` (`username`),
-  KEY `idRev` (`idRev`),
-  KEY `idDest` (`idDest`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `request`
---
-
-INSERT INTO `request` (`idReq`, `type`, `username`, `idRev`, `idDest`) VALUES
-(11, 'destination added', 'superuser', NULL, 25),
-(12, 'destination added', 'superuser', NULL, 26);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `review`
---
-
-DROP TABLE IF EXISTS `review`;
-CREATE TABLE IF NOT EXISTS `review` (
-  `idRev` int(11) NOT NULL AUTO_INCREMENT,
-  `content` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `upCount` int(11) NOT NULL,
-  `downCount` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `idImg` int(11) DEFAULT NULL,
-  `idDest` int(11) NOT NULL,
-  PRIMARY KEY (`idRev`),
-  KEY `idImg` (`idImg`),
-  KEY `username` (`username`),
-  KEY `idDest` (`idDest`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `review`
---
-
-INSERT INTO `review` (`idRev`, `content`, `upCount`, `downCount`, `date`, `username`, `idImg`, `idDest`) VALUES
-(53, '1111', 1, 1, '2018-06-04 00:00:00', 'superuser', NULL, 12),
-(55, 'pozitivan rev', 2, 0, '2018-06-04 00:00:00', 'superuser', NULL, 12),
-(56, 'negativan rev', 0, 2, '2018-06-04 00:00:00', 'superuser', NULL, 12),
-(57, '1111', 0, 1, '2018-06-04 00:00:00', 'user', NULL, 12),
-(58, 'review', 0, 1, '2018-06-04 00:00:00', 'user', 73, 12),
-(59, 'review bez slike', 0, 1, '2018-06-04 00:00:00', 'user', NULL, 12);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `statistic`
---
-
-DROP TABLE IF EXISTS `statistic`;
-CREATE TABLE IF NOT EXISTS `statistic` (
-  `date` datetime NOT NULL,
-  `userCount` int(11) NOT NULL,
-  `reviewCount` int(11) NOT NULL,
-  `destinationCount` int(11) NOT NULL,
-  `positiveVoteCount` int(11) NOT NULL,
-  `negativeVoteCount` int(11) NOT NULL,
-  PRIMARY KEY (`date`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `statistic`
---
-
-INSERT INTO `statistic` (`date`, `userCount`, `reviewCount`, `destinationCount`, `positiveVoteCount`, `negativeVoteCount`) VALUES
-('2018-06-04 00:00:00', 3, 6, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -164,8 +28,7 @@ INSERT INTO `statistic` (`date`, `userCount`, `reviewCount`, `destinationCount`,
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
   `status` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
@@ -173,10 +36,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `firstname` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `lastname` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `gender` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `idImg` int(11) DEFAULT NULL,
-  PRIMARY KEY (`username`),
-  UNIQUE KEY `email` (`email`),
-  KEY `idImg` (`idImg`)
+  `idImg` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -184,75 +44,38 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`username`, `email`, `status`, `password`, `firstname`, `lastname`, `gender`, `idImg`) VALUES
-('admin', 'admin@o3enzyme.com', 'admin', '$2y$10$swhPDdt8Wet90ZUTDxl9nu/ixf71FjpLoPaJuEYK/VlCa3/glnoh2', 'Admin', 'Admin', 'male', NULL),
-('markohudomal', 'markohudomal@yahoo.com', 'admin', '$2y$10$jSkVxQe5Yd.7ZPAGP6ZB3OV5LEAmRdbh8RylCb6tHjoU3NjE2btx2', 'Marko', 'Hudomal', 'male', NULL),
-('superuser', 'superuser@fxprix.com', 'super_user', '$2y$10$opt2L0zHRTF6fKrEAEOapeyrfHVcWzQ8SRAKNuuJoQMms2UGXYIBK', 'SuperUser', 'SuperUser', 'male', NULL),
-('user', 'user@loketa.com', 'user', '$2y$10$LTlkmHSvwjiyFKPcJxZapOfP76nQuY1pp9/SMOWHKkB2/Dh4pohTC', 'User', 'User', 'female', NULL),
-('user2', 'user2@sfamo.com', 'user', '$2y$10$ySfS1xkG8gSk2tA47lvu4u1JwRUtkKI7FGUy7lE0On4g5jvZNaoe2', 'User2', 'User2', 'male', NULL),
-('user3', 'user3@o3enzyme.com', 'user', '$2y$10$r0x3a452DmYR1dc4tZ/D9.brzrZGZMK7AXks1wdQlFqLvgox5S0uy', 'User3', 'User3', 'female', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `vote`
---
-
-DROP TABLE IF EXISTS `vote`;
-CREATE TABLE IF NOT EXISTS `vote` (
-  `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `idRev` int(11) NOT NULL,
-  `type` tinyint(1) NOT NULL,
-  PRIMARY KEY (`username`,`idRev`),
-  KEY `idRev` (`idRev`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+('arnold', 'mikaitata@gmail.com', 'admin', '$2y$10$It83ttYP0PNyVNmihjUd7.6cX5iaK2wijTspE1JZz0qMk/krs47Qi', 'Milos', 'Matijasevic', 'male', 95),
+('ciric_d', 'dejanciric2013@yahoo.com', 'super_user', '$2y$10$GUqGFx01SzmQjpJRAOg9PezWwH29Z/2UiC.sExm7YTW.dnIUq365q', 'Dejan', 'Ciric', 'male', 91),
+('hudi', 'markohudomal@gmail.com', 'super_user', '$2y$10$KOmk2Y4ovNzISpdDThozReMp4SzjfC7hl60fgHm0K7kl9ElCye46.', 'Marko', 'Hudomal', 'male', 80),
+('jakovj', 'jakovjezdic@gmail.com', 'admin', '$2y$10$6sdxB.IALnKHiXEJ8326DurEX3ZorlY2f8KD5mFzHIJyHbxPi42Nm', 'Jakov', 'Jezdić', 'male', 83),
+('jaksaj', 'jaksajezdic03@gmail.com', 'user', '$2y$10$/xu.2B42yIdyfi0hq1HfqOgfLq5oKmT3PE/no9YFTujqr1bss2vvS', 'Jaksa', 'Jezdic', 'male', 99),
+('jana', 'jjanakragovic@gmail.com', 'user', '$2y$10$FpcOBO2jwk9JzAJmzkVbauOX/9G2vw.EpxA0hiMwgwWMXUgydZz1S', 'jana', 'kragovic', 'female', NULL),
+('milos96', 'tetraktis13@gmail.com', 'user', '$2y$10$s7V.fOB4SGKn/GXPmdC0Me7HRRBehdxa9CoMfztIAM4hHAjkAASRG', 'Milos', 'Cubrilo', 'male', NULL),
+('sanja', 'sanja996@live.com', 'user', '$2y$10$W7lYkp7tfoBzXpin5/O.l.By3OVim0ZDzRtGDCg4TT53ISn6sT862', 'Sanja', 'Perisic', 'female', NULL),
+('sara6', 'sacasara6@gmail.com', 'user', '$2y$10$pfPj0tYXzuccIA6Ngdqf0e82D/S6u.zxId3jQ/kgM4exHWSpN3xoG', 'Sara', 'Vukas', 'female', NULL),
+('Veljko966', 'radioheadthom444@gmail.com', 'user', '$2y$10$Jqe9OBktMu5vCKvsJVsz9u/2gY0w85qU.rQ99IHjEeP.hONdDnUYO', 'Veljko', 'Djordjevic', 'male', 93);
 
 --
--- Dumping data for table `vote`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `vote` (`username`, `idRev`, `type`) VALUES
-('admin', 53, -1),
-('admin', 55, 1),
-('admin', 56, -1),
-('admin', 57, -1),
-('admin', 58, -1),
-('admin', 59, -1),
-('user', 53, 1),
-('user', 55, 1),
-('user', 56, -1);
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `idImg` (`idImg`);
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `request`
---
-ALTER TABLE `request`
-  ADD CONSTRAINT `idDest2` FOREIGN KEY (`idDest`) REFERENCES `destination` (`idDest`) ON DELETE CASCADE,
-  ADD CONSTRAINT `idRev1` FOREIGN KEY (`idRev`) REFERENCES `review` (`idRev`) ON DELETE CASCADE,
-  ADD CONSTRAINT `username3` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `review`
---
-ALTER TABLE `review`
-  ADD CONSTRAINT `idDest1` FOREIGN KEY (`idDest`) REFERENCES `destination` (`idDest`) ON DELETE CASCADE,
-  ADD CONSTRAINT `idImg1` FOREIGN KEY (`idImg`) REFERENCES `image` (`idImg`),
-  ADD CONSTRAINT `username1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `idImg` FOREIGN KEY (`idImg`) REFERENCES `image` (`idImg`);
-
---
--- Constraints for table `vote`
---
-ALTER TABLE `vote`
-  ADD CONSTRAINT `idRev` FOREIGN KEY (`idRev`) REFERENCES `review` (`idRev`) ON DELETE CASCADE,
-  ADD CONSTRAINT `username2` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
